2371af4a8c876ec309735fc3e8c15a5f056c853e
shwetaterkar <81641786+shwetaterkar@users.noreply.github.com>
2021-12-30 19:18:23 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,17 @@
-// Time Complexity :
+// Time Complexity : O(n) where n is length of the array
 // Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int rob(int[] nums) {
+      int len=nums.length;
+          int sum= nums[0];
+      for(int i=2;i<len;i+=2){
+          sum=sum+nums[i];
+      } 
+        return sum;
+    }
+}
